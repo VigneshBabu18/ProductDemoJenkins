5a40a8fa800e69f143a1dc7c8c5bb29c0dbb9ae1
VigneshBabu18 <vigneshbabu2021@gmail.com>
2025-08-25 15:35:00 +0530

    init

---
+++ ProductJenkins/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagementApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductManagementApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private static List<Product> products = new List<Product>();
+        private static int nextId = 1;
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            product.Id = nextId++;
+            products.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == updatedProduct.Id);
+            if (product == null) return NotFound();
+
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            product.Category = updatedProduct.Category;
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            return View(product);
+        }
+
+        [HttpDelete, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            products.Remove(product);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
